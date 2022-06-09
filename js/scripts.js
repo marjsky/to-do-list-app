8c27d702a12f2336649c04dadc793bc19d9ab7c0
marjsky <67877013+marjsky@users.noreply.github.com>
2022-06-09 07:36:23 -0400

    clean code for add item to list of items

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,7 @@ function newItem() {
     //1 Adding a new item to the list of items:
     let li = $('<li></li>');
     let inputValue = $('#input').val();
-    let text = $('button').append(document.createTextNode(inputValue));
-    li.append(text);
+    li.append(inputValue);
 
     if (inputValue === '') {
         alert('You must write something!');
